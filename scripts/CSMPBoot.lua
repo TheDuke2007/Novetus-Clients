674a8f6d5e04b249ae992bfc79510d8bdb38bb9e
TheDuke2007 <111716303+TheDuke2007@users.noreply.github.com>
2022-10-05 15:56:04 -0600

    Add files via upload

---
+++ scripts/CSMPBoot.lua
@@ -0,0 +1,3 @@
+--Load Script
+dofile('rbxasset://scripts/CSMPFunctions.lua')
+_G.CSSolo(88,'TheDuke2007','RedBaseballCap.rbxm','NoHat.rbxm','NoHat.rbxm',24,28,24,24,23,23,'NoTShirt.rbxm','StanfordHoodie.rbxm','Jeans.rbxm','DefaultFace.rbxm','DefaultHead.rbxm','NBC','NoExtra.rbxm',false);
